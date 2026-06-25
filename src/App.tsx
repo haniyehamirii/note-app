9930269e2f1c3bd2c00115b97bc5d2cd9803a160
haniyeh amiri <amirihaniyhe@gmail.com>
2026-06-25 23:09:25 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl text-pink-400 font-bold font-sans">note app</h1>
+    </div>
+  )
+}
+
+export default App
